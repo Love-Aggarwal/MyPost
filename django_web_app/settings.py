9e76de52779ccf75af142a6c19710c167b7af2c4
Love-Aggarwal/credit <aggarwallovedec1998@gmail.com>
2021-05-26 20:25:36 +0530

    adding files

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -135,8 +135,10 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
