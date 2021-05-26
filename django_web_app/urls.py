86a452cb3748f96b9e14c1902107e6cbed2ba6e4
Love-Aggarwal <47328559+Love-Aggarwal@users.noreply.github.com>
2021-05-26 21:02:07 +0530

    Update urls.py

--- django_web_app/urls.py
+++ django_web_app/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,6 +34,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
