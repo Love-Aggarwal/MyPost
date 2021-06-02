f974ae0728d90bc6afdaa4d792a9068649a95c62
Love-Aggarwal <47328559+Love-Aggarwal@users.noreply.github.com>
2021-06-02 23:03:14 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -66,7 +66,7 @@ def password_reset_request(request):
 					email_template_name = "users/password_reset_email.txt"
 					c = {
 					"email":user.email,
-					'domain':'127.0.0.1:8000',
+					'domain':'mypost24.herokuapp.com',
 					'site_name': 'Website',
 					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
 					"user": user,
