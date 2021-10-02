cad57aa55d59877af167dd995d994ea3a22022da
Love-Aggarwal <47328559+Love-Aggarwal@users.noreply.github.com>
2021-10-02 13:14:45 +0530

    Update settings.py

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -145,7 +145,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
